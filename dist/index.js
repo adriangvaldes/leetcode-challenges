0c3b924d4bec1a418a63a74fba05cc0df233f719
Adrian Valdes <adriangvaldes@gmail.com>
2025-09-14 03:36:06 -0300

    finished remove-duplicates challenge

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,12 @@
-function removeDuplicates(numbers) {
-  console.log("teste");
+"use strict";
+function removeDuplicates(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    console.log(nums);
+    return nums.length;
 }
-removeDuplicates([1]);
+removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
