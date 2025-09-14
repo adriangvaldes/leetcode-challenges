05d91379fe4cdbea162029840a408d87753ff94a
Adrian Valdes <adriangvaldes@gmail.com>
2025-09-14 03:37:16 -0300

    refact: files organization changes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-function removeDuplicates(nums: Number[]): number {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1]) {
-      nums.splice(i, 1);
-      i--;
-    }
-  }
-
-  return nums.length;
-}
+import { removeDuplicates } from "./challenges/removeDuplicates";
 
 removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
